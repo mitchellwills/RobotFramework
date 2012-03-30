8e62a08904d3fa73dd0f8ed9f7d1d9b71c745974
Mitchell Wills <mitchellweb1@gmail.com>
2012-03-30 10:27:18 -0400

    Reverted Pin configurations to arduino pin style

--- Imperium/src/robot/imperium/hardware/ArduinoDuemilanove.java
+++ Imperium/src/robot/imperium/hardware/ArduinoDuemilanove.java
@@ -12,26 +12,6 @@ import static robot.imperium.PinCapability.SPI_MOSI;
 import static robot.imperium.PinCapability.SPI_SCK;
 import static robot.imperium.PinCapability.SPI_SS;
 import static robot.imperium.PinCapability.SelectablePullUp;
-import static robot.imperium.hardware.AtmegaPins.PB0;
-import static robot.imperium.hardware.AtmegaPins.PB1;
-import static robot.imperium.hardware.AtmegaPins.PB2;
-import static robot.imperium.hardware.AtmegaPins.PB3;
-import static robot.imperium.hardware.AtmegaPins.PB4;
-import static robot.imperium.hardware.AtmegaPins.PB5;
-import static robot.imperium.hardware.AtmegaPins.PC0;
-import static robot.imperium.hardware.AtmegaPins.PC1;
-import static robot.imperium.hardware.AtmegaPins.PC2;
-import static robot.imperium.hardware.AtmegaPins.PC3;
-import static robot.imperium.hardware.AtmegaPins.PC4;
-import static robot.imperium.hardware.AtmegaPins.PC5;
-import static robot.imperium.hardware.AtmegaPins.PD0;
-import static robot.imperium.hardware.AtmegaPins.PD1;
-import static robot.imperium.hardware.AtmegaPins.PD2;
-import static robot.imperium.hardware.AtmegaPins.PD3;
-import static robot.imperium.hardware.AtmegaPins.PD4;
-import static robot.imperium.hardware.AtmegaPins.PD5;
-import static robot.imperium.hardware.AtmegaPins.PD6;
-import static robot.imperium.hardware.AtmegaPins.PD7;
 
 /**
  * @author Mitchell
@@ -52,47 +32,47 @@ public class ArduinoDuemilanove extends HardwareConfiguration {
 	private ArduinoDuemilanove() {
 		super("Arduino Duemilanove", 490);
 
-		addPin(PD0, "0").addLabels("PD0", "TX");
+		addPin(0, "0").addLabels("PD0", "TX");
 		
-		addPin(PD1, "1").addLabels("PD1", "RX");
+		addPin(1, "1").addLabels("PD1", "RX");
 		
-		addPin(PD2, "2", DigitalInput, DigitalOutput, SelectablePullUp, MSPWM_Output).addLabels("PD2", "INT0");
+		addPin(2, "2", DigitalInput, DigitalOutput, SelectablePullUp, MSPWM_Output).addLabels("PD2", "INT0");
 		
-		addPin(PD3, "3", DigitalInput, DigitalOutput, SelectablePullUp, MSPWM_Output, PWM_Output).addLabels("PD3", "INT1");
+		addPin(3, "3", DigitalInput, DigitalOutput, SelectablePullUp, MSPWM_Output, PWM_Output).addLabels("PD3", "INT1");
 		
-		addPin(PD4, "4", DigitalInput, DigitalOutput, SelectablePullUp, MSPWM_Output).addLabels("PD4", "T0");
+		addPin(4, "4", DigitalInput, DigitalOutput, SelectablePullUp, MSPWM_Output).addLabels("PD4", "T0");
 		
-		addPin(PD5, "5", DigitalInput, DigitalOutput, SelectablePullUp, MSPWM_Output, PWM_Output).addLabels("PD5", "T1");
+		addPin(5, "5", DigitalInput, DigitalOutput, SelectablePullUp, MSPWM_Output, PWM_Output).addLabels("PD5", "T1");
 		
-		addPin(PD6, "6", DigitalInput, DigitalOutput, SelectablePullUp, MSPWM_Output, PWM_Output).addLabels("PD6", "AIN0");
+		addPin(6, "6", DigitalInput, DigitalOutput, SelectablePullUp, MSPWM_Output, PWM_Output).addLabels("PD6", "AIN0");
 		
-		addPin(PD7, "7", DigitalInput, DigitalOutput, SelectablePullUp, MSPWM_Output).addLabels("PD7", "AIN1");
+		addPin(7, "7", DigitalInput, DigitalOutput, SelectablePullUp, MSPWM_Output).addLabels("PD7", "AIN1");
 		
-		addPin(PB0, "8", DigitalInput, DigitalOutput, SelectablePullUp, MSPWM_Output).addLabels("PB0", "ICP");
+		addPin(8, "8", DigitalInput, DigitalOutput, SelectablePullUp, MSPWM_Output).addLabels("PB0", "ICP");
 		
-		addPin(PB1, "9", DigitalInput, DigitalOutput, SelectablePullUp, MSPWM_Output, PWM_Output).addLabels("PB1", "OC1");
+		addPin(9, "9", DigitalInput, DigitalOutput, SelectablePullUp, MSPWM_Output, PWM_Output).addLabels("PB1", "OC1");
 		
-		addPin(PB2, "10", DigitalInput, DigitalOutput, SelectablePullUp, MSPWM_Output, PWM_Output, SPI_SS).addLabels("PB2", "SS");
+		addPin(10, "10", DigitalInput, DigitalOutput, SelectablePullUp, MSPWM_Output, PWM_Output, SPI_SS).addLabels("PB2", "SS");
 		
-		addPin(PB3, "11", DigitalInput, DigitalOutput, SelectablePullUp, MSPWM_Output, PWM_Output, SPI_MOSI).addLabels("PB3", "MOSI");
+		addPin(11, "11", DigitalInput, DigitalOutput, SelectablePullUp, MSPWM_Output, PWM_Output, SPI_MOSI).addLabels("PB3", "MOSI");
 		
-		addPin(PB4, "12", DigitalInput, DigitalOutput, SelectablePullUp, MSPWM_Output, SPI_MISO).addLabels("PB4", "MISO");
+		addPin(12, "12", DigitalInput, DigitalOutput, SelectablePullUp, MSPWM_Output, SPI_MISO).addLabels("PB4", "MISO");
 		
-		addPin(PB5, "13", DigitalInput, DigitalOutput, SelectablePullUp, MSPWM_Output, SPI_SCK).addLabels("PB5", "SCK", HardwareConfiguration.ONBOARD_LED);
+		addPin(13, "13", DigitalInput, DigitalOutput, SelectablePullUp, MSPWM_Output, SPI_SCK).addLabels("PB5", "SCK", HardwareConfiguration.ONBOARD_LED);
 		
 		
 
-		addPin(PC0, "A0", DigitalInput, DigitalOutput, SelectablePullUp, MSPWM_Output, AnalogInput).addLabels("PC0", "ADC0");
+		addPin(14, "A0", DigitalInput, DigitalOutput, SelectablePullUp, MSPWM_Output, AnalogInput).addLabels("PC0", "ADC0");
 		
-		addPin(PC1, "A1", DigitalInput, DigitalOutput, SelectablePullUp, MSPWM_Output, AnalogInput).addLabels("PC1", "ADC1");
+		addPin(15, "A1", DigitalInput, DigitalOutput, SelectablePullUp, MSPWM_Output, AnalogInput).addLabels("PC1", "ADC1");
 		
-		addPin(PC2, "A2", DigitalInput, DigitalOutput, SelectablePullUp, MSPWM_Output, AnalogInput).addLabels("PC2", "ADC2");
+		addPin(16, "A2", DigitalInput, DigitalOutput, SelectablePullUp, MSPWM_Output, AnalogInput).addLabels("PC2", "ADC2");
 		
-		addPin(PC3, "A3", DigitalInput, DigitalOutput, SelectablePullUp, MSPWM_Output, AnalogInput).addLabels("PC3", "ADC3");
+		addPin(17, "A3", DigitalInput, DigitalOutput, SelectablePullUp, MSPWM_Output, AnalogInput).addLabels("PC3", "ADC3");
 		
-		addPin(PC4, "A4", DigitalInput, DigitalOutput, SelectablePullUp, MSPWM_Output, AnalogInput, I2C_SDA).addLabels("PC4", "ADC4", "SDA");
+		addPin(18, "A4", DigitalInput, DigitalOutput, SelectablePullUp, MSPWM_Output, AnalogInput, I2C_SDA).addLabels("PC4", "ADC4", "SDA");
 		
-		addPin(PC5, "A5", DigitalInput, DigitalOutput, SelectablePullUp, MSPWM_Output, AnalogInput, I2C_SCL).addLabels("PC5", "ADC5", "SCL");
+		addPin(19, "A5", DigitalInput, DigitalOutput, SelectablePullUp, MSPWM_Output, AnalogInput, I2C_SCL).addLabels("PC5", "ADC5", "SCL");
 	}
 
 }
--- Imperium/src/robot/imperium/hardware/ArduinoMega2560.java
+++ Imperium/src/robot/imperium/hardware/ArduinoMega2560.java
@@ -5,20 +5,6 @@ import static robot.imperium.PinCapability.DigitalOutput;
 import static robot.imperium.PinCapability.MSPWM_Output;
 import static robot.imperium.PinCapability.PWM_Output;
 import static robot.imperium.PinCapability.SelectablePullUp;
-import static robot.imperium.hardware.AtmegaPins.PB4;
-import static robot.imperium.hardware.AtmegaPins.PB5;
-import static robot.imperium.hardware.AtmegaPins.PB6;
-import static robot.imperium.hardware.AtmegaPins.PB7;
-import static robot.imperium.hardware.AtmegaPins.PE0;
-import static robot.imperium.hardware.AtmegaPins.PE1;
-import static robot.imperium.hardware.AtmegaPins.PE3;
-import static robot.imperium.hardware.AtmegaPins.PE4;
-import static robot.imperium.hardware.AtmegaPins.PE5;
-import static robot.imperium.hardware.AtmegaPins.PG5;
-import static robot.imperium.hardware.AtmegaPins.PH3;
-import static robot.imperium.hardware.AtmegaPins.PH4;
-import static robot.imperium.hardware.AtmegaPins.PH5;
-import static robot.imperium.hardware.AtmegaPins.PH6;
 
 /**
  * @author Mitchell
@@ -39,25 +25,25 @@ public class ArduinoMega2560 extends HardwareConfiguration {
 	private ArduinoMega2560() {
 		super("Arduino Duemilanove", 490);
 
-		addPin(PE0, "0").addLabels("PE0", "RX0");
-		addPin(PE1, "1").addLabels("PE1", "TX0");
+		addPin(0, "0").addLabels("PE0", "RX0");
+		addPin(1, "1").addLabels("PE1", "TX0");
 
-		addPin(PE4, "2", DigitalInput, DigitalOutput, SelectablePullUp, MSPWM_Output, PWM_Output).addLabels("PE4");
-		addPin(PE5, "3", DigitalInput, DigitalOutput, SelectablePullUp, MSPWM_Output, PWM_Output).addLabels("PE5");
+		addPin(2, "2", DigitalInput, DigitalOutput, SelectablePullUp, MSPWM_Output, PWM_Output).addLabels("PE4");
+		addPin(3, "3", DigitalInput, DigitalOutput, SelectablePullUp, MSPWM_Output, PWM_Output).addLabels("PE5");
 		
-		addPin(PG5, "4", DigitalInput, DigitalOutput, SelectablePullUp, MSPWM_Output, PWM_Output).addLabels("PG5");
+		addPin(4, "4", DigitalInput, DigitalOutput, SelectablePullUp, MSPWM_Output, PWM_Output).addLabels("PG5");
 
-		addPin(PE3, "5", DigitalInput, DigitalOutput, SelectablePullUp, MSPWM_Output, PWM_Output).addLabels("PE3");
+		addPin(5, "5", DigitalInput, DigitalOutput, SelectablePullUp, MSPWM_Output, PWM_Output).addLabels("PE3");
 
-		addPin(PH3, "6", DigitalInput, DigitalOutput, SelectablePullUp, MSPWM_Output, PWM_Output).addLabels("PH3");
-		addPin(PH4, "7", DigitalInput, DigitalOutput, SelectablePullUp, MSPWM_Output, PWM_Output).addLabels("PH4");
-		addPin(PH5, "8", DigitalInput, DigitalOutput, SelectablePullUp, MSPWM_Output, PWM_Output).addLabels("PH5");
-		addPin(PH6, "9", DigitalInput, DigitalOutput, SelectablePullUp, MSPWM_Output, PWM_Output).addLabels("PH6");
+		addPin(6, "6", DigitalInput, DigitalOutput, SelectablePullUp, MSPWM_Output, PWM_Output).addLabels("PH3");
+		addPin(7, "7", DigitalInput, DigitalOutput, SelectablePullUp, MSPWM_Output, PWM_Output).addLabels("PH4");
+		addPin(8, "8", DigitalInput, DigitalOutput, SelectablePullUp, MSPWM_Output, PWM_Output).addLabels("PH5");
+		addPin(9, "9", DigitalInput, DigitalOutput, SelectablePullUp, MSPWM_Output, PWM_Output).addLabels("PH6");
 		
-		addPin(PB4, "10", DigitalInput, DigitalOutput, SelectablePullUp, MSPWM_Output, PWM_Output).addLabels("PB4");
-		addPin(PB5, "11", DigitalInput, DigitalOutput, SelectablePullUp, MSPWM_Output, PWM_Output).addLabels("PB5");
-		addPin(PB6, "12", DigitalInput, DigitalOutput, SelectablePullUp, MSPWM_Output, PWM_Output).addLabels("PB6");
-		addPin(PB7, "13", DigitalInput, DigitalOutput, SelectablePullUp, MSPWM_Output, PWM_Output).addLabels("PB7", ONBOARD_LED);
+		addPin(10, "10", DigitalInput, DigitalOutput, SelectablePullUp, MSPWM_Output, PWM_Output).addLabels("PB4");
+		addPin(11, "11", DigitalInput, DigitalOutput, SelectablePullUp, MSPWM_Output, PWM_Output).addLabels("PB5");
+		addPin(12, "12", DigitalInput, DigitalOutput, SelectablePullUp, MSPWM_Output, PWM_Output).addLabels("PB6");
+		addPin(13, "13", DigitalInput, DigitalOutput, SelectablePullUp, MSPWM_Output, PWM_Output).addLabels("PB7", ONBOARD_LED);
 		
 		
 
