eb29ca544d76e6b2a6aeecec97bc54d4e5681947
Mitchell Wills <mitchellweb1@gmail.com>
2012-03-31 21:38:00 -0400

    Added Thread for PID loop update

--- RobotFramework/src/robot/pid/PIDController.java
+++ RobotFramework/src/robot/pid/PIDController.java
@@ -19,6 +19,8 @@ public class PIDController implements PIDLoop{
 	private final double minOutput;
 	private final double maxOutput;
 	
+	private final long updateDelay;
+	
 	private double setpoint;
 	
 
@@ -33,6 +35,20 @@ public class PIDController implements PIDLoop{
 	 */
 	public PIDController(double Kp, double Ki, double Kd, PIDInput input,
 			PIDOutput output, double minOutput, double maxOutput) {
+		this(Kp, Ki, Kd, input, output, minOutput, maxOutput, 20);
+	}
+	/**
+	 * @param Kp
+	 * @param Ki
+	 * @param Kd
+	 * @param input
+	 * @param output
+	 * @param minOutput 
+	 * @param maxOutput 
+	 * @param updateDelay the delay between updates of the PID loop
+	 */
+	public PIDController(double Kp, double Ki, double Kd, PIDInput input,
+			PIDOutput output, double minOutput, double maxOutput, long updateDelay) {
 		this.Kp = Kp;
 		this.Ki = Ki;
 		this.Kd = Kd;
@@ -40,13 +56,22 @@ public class PIDController implements PIDLoop{
 		this.output = output;
 		this.minOutput = minOutput;
 		this.maxOutput = maxOutput;
+		this.updateDelay = updateDelay;
 		
 		setpoint = getPosition();
+		
+		new PIDControllerThread().start();
 	}
 	
+	/**
+	 * perform an update on the pid loop
+	 */
 	public void update(){
 		double error = getPosition() - setpoint;
+		
 		double value = Kp * error;
+		
+		//TODO do integral and derivative terms
 		
 		value = RobotUtil.limit(value, minOutput, maxOutput);
 		output.setPIDOutput(value);
@@ -66,5 +91,18 @@ public class PIDController implements PIDLoop{
 	public double getPosition() {
 		return input.getPIDInput();
 	}
+	
+
+	
+	private class PIDControllerThread extends Thread {
+		@Override
+		public void run(){
+			while(true){
+				update();
+				RobotUtil.sleep(updateDelay);//TODO do calculation for loop to actully run every updateDelay
+			}
+		}
+	}
+
 
 }
