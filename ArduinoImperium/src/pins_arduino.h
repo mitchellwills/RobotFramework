e78ec910cf88e4a614d673bbe11f042dabcfbc5a
Mitchell Wills <mitchellweb1@gmail.com>
2012-04-09 17:52:48 -0400

    select ArduinoImperium pin layout based on target MCU

--- ArduinoImperium/src/pins_arduino.h
+++ ArduinoImperium/src/pins_arduino.h
@@ -9,8 +9,11 @@
 #define PINS_ARDUINO_H_
 
 
+#if defined(__AVR_ATmega328P__)
 #include "pins_arduino_standard.h"
-
+#elif defined(__AVR_ATmega2560__)
+#include "pins_arduino_mega.h"
+#endif
 
 
 #endif /* PINS_ARDUINO_H_ */
