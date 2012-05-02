aebb950377765ad50a6ad93c1e2dde87a32eb084
Mitchell Wills <mitchellweb1@gmail.com>
2012-05-02 01:08:40 -0400

    Added utility methods to robot util

--- RobotFramework/src/robot/util/RobotUtil.java
+++ RobotFramework/src/robot/util/RobotUtil.java
@@ -47,4 +47,28 @@ public class RobotUtil {
 	public static boolean within(double value, double min, double max) {
 		return min<value && value<max;
 	}
+	
+	/**
+	 * Take the input value and return it or the default value if the input value is between the min and max values
+	 * @param value the input value
+	 * @param min the minimum value
+	 * @param max the maximum value
+	 * @param defaultValue the value returned if the input is between the min and max values
+	 * @return the thresholded value
+	 */
+	public static double threshold(double value, double min, double max, double defaultValue){
+		if(within(value, min, max))
+			return defaultValue;
+		return value;
+	}
+	
+	/**
+	 * Threshold the input value around 0
+	 * @param value the input value
+	 * @param threshold the amount to threshold around 0 (bounds are -threshold to threshold)
+	 * @return the thresholded value
+	 */
+	public static double threshold(double value, double threshold){
+		return threshold(value, -threshold, threshold, 0);
+	}
 }
